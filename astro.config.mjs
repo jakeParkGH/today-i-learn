a4c41a75cf0916ca22ee9c82112114e93c84e6e4
jake <pjklogs@naver.com>
2025-04-28 01:30:53 +0900

    chore: 배포를 위한 환경변수 & 패키지 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-// @ts-check
+import { loadEnv } from 'vite';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import { fileURLToPath } from 'url';
 
+const { PUBLIC_SITE_URL } = loadEnv(process.env.PUBLIC_SITE_URL, process.cwd(), '');
+const { PUBLIC_SITE_BASE } = loadEnv(process.env.PUBLIC_SITE_BASE, process.cwd(), '');
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: PUBLIC_SITE_URL,
+  base: PUBLIC_SITE_BASE,
   integrations: [mdx(), sitemap(), react()],
   vite: {
     resolve: {
